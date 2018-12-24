e5f750fcf73ea9cd8eb73613132eb26822dcab70
B V Sathya Rakesh Kumar <sathyarakesh@ixed-lap0196.i-exceed.com>
2018-12-24 15:03:46 +0530

    Initial Commit

---
+++ GithubPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubPractice
+//
+//  Created by  B V Sathya Rakesh Kumar on 24/12/18.
+//  Copyright © 2018  B V Sathya Rakesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
